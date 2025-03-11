7b1c291294b78b5f8e6e0ac365c5c9e63d7db67b
sylee <vidafirmar@gmail.com>
2025-03-11 17:25:01 +0900

    add: timer for inquiry btc price inverval

--- pythrading/internal/gui/qt.py
+++ pythrading/internal/gui/qt.py
@@ -1,29 +1,43 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
+from PyQt5.QtCore import *
 import os
 import pykorbit
 # form_class = uic.loadUiType(os.path.join(os.path.dirname(__file__), "mywindow.ui"))[0]
 form_class = uic.loadUiType("/Users/isang-yong/Desktop/mywindow.ui")[0]
+
+class MySignal(QObject):
+    signal1 = pyqtSignal(str)
+
+    def run(self):
+        self.signal1.emit("hello world")
 
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # UI 설정
         self.setWindowTitle("bitcoin auto trader")
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.inquiry)
         # self.setWindowIcon(QIcon("bitcoin.png")) // not wokring on mac
 
         # 버튼 클릭 이벤트 연결
         self.inquiryButton.clicked.connect(self.inquiry)
 
+        mysignal = MySignal()
+        mysignal.signal1.connect(self.signal1_emitted)
+        mysignal.run()
+
     def inquiry(self):
+        cur_time = QTime.currentTime()
+        str_time = cur_time.toString("hh:mm:ss")
+        self.statusBar().showMessage(str_time)
         price: float = pykorbit.get_current_price("BTC")
+
         price = round(price)  # 반올림
-        print(price)
         strPrice: str = str(price)
-        # 1000단위 이상이면 1000단위마다 컴마찍어서 구분하기
-        # if len(strPrice) > 3:
-        print(strPrice)
         newPrice: str = ""
         if len(strPrice) > 3:
             for i in range(len(strPrice)):
@@ -32,3 +46,7 @@ class MyWindow(QMainWindow, form_class):
                 else:
                     newPrice += strPrice[i]
         self.currentPriceEdit.setText(newPrice)
+
+    @pyqtSlot()
+    def signal1_emitted(self):
+        print("signal1_emitted")
